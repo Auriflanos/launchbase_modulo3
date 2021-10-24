cd691e9c05ff7f7ae1cfec194ade1ab7f36dd7a5
Lucas Gonçalves <66454089+Auriflanos@users.noreply.github.com>
2021-10-24 14:33:56 +0200

    Addes Files

---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: "GykTLq0DQuU",
+    title: "Formulário animado com JS puro e CSS animation / Mayk Brito",
+    duration: "57 min",
+    price: "Free"
+  },
+  {
+    id: "HN1UjzRSb8k",
+    title: "Desvendando o CSS grid na prática / Mayk Brito",
+    duration: "36 min",
+    price: "Free"
+  },
+  {
+    id: "rAzHvYnQ8DY",
+    title: "ArrayÇ Highter Order Funcions / Mayk Brito",
+    duration: "54 min",
+    price: "Free"
+  }
+]
